36540bc32b0c3b57ce5cd5b5ed52ecdd5da4e1f1
Francesco <scovascova@gmail.com>
2021-11-15 15:59:00 +0100

    soluzione Jsnack1 con ciclo while

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // JSnack 1
 
-let somma = 0;
-for(i = 0; i < 5; i++){
-    const insNum = parseInt(prompt('Inserisci un numero'));
-    console.log(insNum); 
+// soluzione con for
+// let somma = 0;
+// for(let i = 0; i < 5; i++){
+//     const insNum = parseInt(prompt('Inserisci un numero'));
+    
+//     somma += insNum;
+//     console.log(somma)
+// }
 
+// soluzione con while
+let somma = 0
+
+let i = 0;
+while(i < 5){
+    i++;
+    const insNum = parseInt(prompt('Inserisci un numero'));
+    
+    somma += insNum;
+    console.log(somma)
 }
 
