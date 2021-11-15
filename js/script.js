c9da36a9957ec4ba8e70e7e1143691405ee211f2
Francesco <scovascova@gmail.com>
2021-11-15 16:18:55 +0100

    Jsnack 2

--- js/script.js
+++ js/script.js
@@ -10,14 +10,32 @@
 // }
 
 // soluzione con while
-let somma = 0
+// let somma = 0
 
-let i = 0;
-while(i < 5){
-    i++;
-    const insNum = parseInt(prompt('Inserisci un numero'));
+// let i = 0;
+// while(i < 5){
+//     i++;
+//     const insNum = parseInt(prompt('Inserisci un numero'));
     
-    somma += insNum;
-    console.log(somma)
+//     somma += insNum;
+//     console.log(somma)
+// }
+
+
+
+// Jsnack2
+const parola1 = prompt('Inserisci parola 1');
+const parola2 = prompt('Inserisci parola 2');
+
+let lunghezza1 = parola1.length;
+let lunghezza2 = parola2.length;
+
+if(lunghezza1 > lunghezza2){
+    console.log(parola1);
+}else if(lunghezza2 > lunghezza1){
+    console.log(parola2);
+}else{
+    console.log(parola1);
+    console.log(parola2);
 }
 
